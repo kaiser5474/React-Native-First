a3ab12583681a26dd13845c75f1e2697b47b8fa1
kaiser5474 <jarvismiranda728096@gmail.com>
2022-05-11 04:17:54 -0400

    Version 0.1

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -5,20 +5,20 @@ import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import { useQuery, gql } from "@apollo/client";
 
-const FILMS_QUERY = gql`
-query {
-  characters(page: 2, filter: { name: "rick" }) {
-    info {
-      count
-    }
-    results {
-      name
-      status
-      image
-    }
-  }
-}
-`;
+// const FILMS_QUERY = gql`
+// query {
+//   characters(page: 2, filter: { name: "rick" }) {
+//     info {
+//       count
+//     }
+//     results {
+//       name
+//       status
+//       image
+//     }
+//   }
+// }
+// `;
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   // const { data, loading, error } = useQuery(FILMS_QUERY);
